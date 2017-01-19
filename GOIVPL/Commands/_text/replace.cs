c88f0798d2b85b71e57a81ee7356554a92cfe76d
Dominik Louven <louven.dominik@gmail.com>
2017-01-19 16:47:33 +0100

    Revert "Merged replace commands"
    This reverts commit 9f906f54499f5f6cf47b08a1e47b6ab720c06cd4.

---
+++ GOIVPL/Commands/_text/replace.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOIVPL.Commands._text
+{
+    [System.Xml.Serialization.XmlType("replaceText")]
+    public class replace : Command
+    {
+        private String line, condition,text;
+
+        [System.Xml.Serialization.XmlText()]
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        [System.Xml.Serialization.XmlAttribute("line")]
+        public string Line
+        {
+            get
+            {
+                return line;
+            }
+
+            set
+            {
+                line = value;
+            }
+        }
+
+        [System.Xml.Serialization.XmlAttribute("condition")]
+        public string Condition
+        {
+            get
+            {
+                return condition;
+            }
+
+            set
+            {
+                condition = value;
+            }
+        }
+
+        public replace() : base()
+        {
+
+        }
+
+        public override string getString()
+        {
+            return "text replace, line=" + line + ", condition=" + condition + ",text=" + text;
+        }
+    }
+}
---
+++ GOIVPL/Commands/_xml/replace.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOIVPL.Commands._xml
+{
+    [System.Xml.Serialization.XmlType("replaceXml")]
+    public class replace : Command
+    {
+        private String xpath;
+
+        [System.Xml.Serialization.XmlAttribute("xpath")]
+        public string XPath
+        {
+            get
+            {
+                return xpath;
+            }
+
+            set
+            {
+                xpath = value;
+            }
+        }
+
+        public replace() : base()
+        {
+
+        }
+
+        public override string getString()
+        {
+            return "xml replace, xpath=" + xpath;
+        }
+    }
+}
