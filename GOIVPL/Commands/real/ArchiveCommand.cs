c0a0364f271dca8905afeb5d138fae30eb22e174
Dominik Louven <louven.dominik@gmail.com>
2017-01-25 19:47:05 +0100

    Implemented  Archive Command

---
+++ GOIVPL/Commands/real/ArchiveCommand.cs
@@ -0,0 +1,41 @@
+﻿using GOIVPL.Commands.generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOIVPL.Commands.real
+{
+    [System.Xml.Serialization.XmlRoot(ElementName = "archive")]
+    public class ArchiveCommand : BaseCommand
+    {
+        public ArchiveCommand() : base()
+        {
+
+        }
+
+        [System.Xml.Serialization.XmlAttribute("path")]
+        public string Path
+        {
+            get; set;
+        }
+
+        [System.Xml.Serialization.XmlAttribute("createIfNotExist")]
+        public string CreateIfNotExists
+        {
+            get; set;
+        }
+
+        [System.Xml.Serialization.XmlAttribute("type")]
+        public string ArchiveType
+        {
+            get; set;
+        }
+
+        public override string getName()
+        {
+            return "Archive, Path : " + Path;
+        }
+    }
+}
